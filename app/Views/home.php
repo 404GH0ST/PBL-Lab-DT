7ad30f61dc264aa7635cf47f13ff6c6c6b7e1558
wawaelenti <irawantielent@gmail.com>
2025-11-15 16:29:10 +0700

    feat(home): add about section

--- app/Views/home.php
+++ app/Views/home.php
@@ -17,3 +17,36 @@
         </div>
     </div>
 </section>
+
+<section id="about">
+    <div class="container py-5">
+        <p class="fw-medium text-center" style="font-size: 16px; color: #5F983C;">Tentang Kami</p>
+        <h2 class="fw-bold about-gradient-text text-center mb-5" style="font-size: 40px;">Profil
+            Laboratorium</h2>
+        <div class="container">
+            <div class="row g-2">
+                <div class="col-lg-6 d-flex align-items-center">
+                    <img src="/assets/images/dt-logo.png" alt="" class="img-fluid" style="height: 250px;">
+                </div>
+                <div class="col-lg-6">
+                    <p class="text-black" style="text-align: justify; line-height: 1.8">
+                        Unit penunjang akademik di Jurusan Teknologi Informasi yang berfokus pada kegiatan pembelajaran,
+                        penelitian, serta pengembangan keilmuan di bidang teknologi berbasis data. Laboratorium ini
+                        menyediakan
+                        fasilitas praktikum dan riset yang mendukung penguasaan pengetahuan serta keterampilan mahasiswa
+                        dalam
+                        pengolahan data, analisis big data, kecerdasan buatan, dan machine learning. Selain sebagai
+                        sarana
+                        praktikum, Laboratorium Teknologi Data juga berperan sebagai pusat penelitian dan pengembangan
+                        bagi dosen
+                        maupun mahasiswa.
+                    </p>
+                    <button class="btn mt-2 px-4 py-2 fw-bold text-white animate-fade rounded-3"
+                        style="animation-delay: .4s; background: linear-gradient(to right, #1FA2FF, #12D8FA, #A6FFCB);">
+                        Selengkapnya
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
