f79fdb3025da1666c1d1eb3a72c265313bf545f7
wawaelenti <irawantielent@gmail.com>
2025-11-20 00:38:32 +0700

    feat: add footer

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -8,6 +8,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -24,8 +25,8 @@
         <?= $content ?? '' ?>
     </main>
 
+    <?php include __DIR__ . '/../components/footer.php'; ?>
+
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-
-</html>
