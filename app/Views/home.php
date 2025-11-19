42e707482fefbd3e3832276c40049351f85f76aa
wawaelenti <irawantielent@gmail.com>
2025-11-20 00:12:35 +0700

    feat: add tim section

--- app/Views/home.php
+++ app/Views/home.php
@@ -7,7 +7,7 @@
         </p>
         <div class="d-flex justify-content-center align-items-center gap-4" style="font-size: 20px;">
             <button class="btn mt-2 px-4 py-2 fw-bold text-white animate-fade"
-                style="animation-delay: .4s; background: linear-gradient(to right, #1FA2FF, #12D8FA, #A6FFCB);">
+                style="animation-delay: .4s; background: #7ABC52;">
                 Explore Research
             </button>
             <button class="btn mt-2 px-4 py-2 fw-bold text-white border animate-fade"
@@ -42,11 +42,49 @@
                         maupun mahasiswa.
                     </p>
                     <button class="btn mt-2 px-4 py-2 fw-bold text-white animate-fade rounded-3"
-                        style="animation-delay: .4s; background: linear-gradient(to right, #1FA2FF, #12D8FA, #A6FFCB);">
+                        style="animation-delay: .4s; background: #7ABC52;">
                         Selengkapnya
                     </button>
                 </div>
             </div>
         </div>
+    </div>
+</section>
+
+<section id="team" style="background: #19586E;">
+    <div class="container py-5" style="min-height: 416px;">
+        <div class="row g-4 align-items-center">
+            <div class="col-lg-5">
+                <div class="d-flex flex-column justify-content-center">
+                    <h2 class="text-white" style="font-size: 16px;">About Us</h2>
+                    <h2 class="text-white" style="font-size: 40px; font-weight: bold;">Anggota Tim</h2>
+                    <button class="btn mt-2 px-4 py-2 fw-bold text-white animate-fade rounded-3"
+                        style="animation-delay: .4s; background: #7ABC52; width: fit-content;">
+                        Selengkapnya
+                    </button>
+                </div>
+            </div>
+            <div class="col-lg-7">
+                <div class="border border-white rounded-3 p-4 d-flex flex-column flex-md-row align-items-center"
+                    style="border-width: 2px !important; height: 318px;">
+
+                    <div class="flex-grow-1 text-center text-md-start order-2 order-md-1">
+                        <h3 class="text-white fw-semibold" style="font-size: 28px;">
+                            Yoppy Yunhasnawa, S.ST., M.Sc.
+                        </h3>
+                        <p class="text-white mb-0" style="font-size: 18px;">
+                            Kepala Lab Teknologi Data
+                        </p>
+                    </div>
+
+                    <div class="mb-3 mb-md-0 ms-md-4 order-1 order-md-2">
+                        <img src="/assets/images/kepala-lab.png" alt="Foto Profile"
+                            class="rounded-circle border border-white"
+                            style="width: 170px; height: 170px; object-fit: cover; border-width: 3px !important;">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     </div>
 </section>
