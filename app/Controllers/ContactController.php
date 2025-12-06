b5f3074a944c997914a9245709ff76280be96295
wawaelenti <irawantielent@gmail.com>
2025-12-06 15:13:54 +0700

    fix: minor changes

--- app/Controllers/ContactController.php
+++ app/Controllers/ContactController.php
@@ -50,7 +50,7 @@ class ContactController
             $mail->Port = 587;
 
             // Sender
-            $mail->setFrom('pbl.labdt@gmail.com', 'Your Website');
+            $mail->setFrom('pbl.labdt@gmail.com', 'PBL Lab DT');
 
             // Receiver
             $mail->addAddress($to);
