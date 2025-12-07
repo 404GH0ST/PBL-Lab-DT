62d4219b2ad2a17499b5dcb1f9c295d22dec2d77
wawaelenti <irawantielent@gmail.com>
2025-12-07 15:27:54 +0700

    feat: implement search news and pagination

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -136,16 +136,29 @@ class HomeController extends Controller
     {
         $page = $_GET['page'] ?? 1;
         $limit = 10;
-        $total = $this->newsModel->countAllNews();
+        $keyword = $_GET['search'] ?? null;
+
+        if ($keyword) {
+            // Count result search
+            $total = $this->newsModel->countSearchNews($keyword);
+        } else {
+            $total = $this->newsModel->countAllNews();
+        }
+
         $pagination = new Pagination($total, $limit, $page);
 
-        $news = $this->newsModel->getApprovedNews($limit, $pagination->getOffset());
+        if ($keyword) {
+            $news = $this->newsModel->searchNews($keyword, $limit, $pagination->getOffset());
+        } else {
+            $news = $this->newsModel->getApprovedNews($limit, $pagination->getOffset());
+        }
 
         return $this->view('news', [
             'title' => 'News - Profile Lab DT',
             'news' => $news,
             'pagination' => $pagination,
-            'baseUrl' => '/news'
+            'baseUrl' => '/news',
+            'keyword' => $keyword
         ]);
     }
 
