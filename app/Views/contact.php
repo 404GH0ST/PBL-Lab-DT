12a2b56410c1eb1f32d9ac1c5910b75a5a5603e4
wawaelenti <irawantielent@gmail.com>
2025-12-08 09:29:43 +0700

    fix: minor changes

--- app/Views/contact.php
+++ app/Views/contact.php
@@ -16,7 +16,7 @@
                     </div>
                     <div>
                         <span class="d-block fw-bold text-dark">Email Us</span>
-                        <span class="text-muted">datatechlab@college.edu</span>
+                        <span class="text-muted">lab.datatech@gmail.com</span>
                     </div>
                 </div>
             </div>
